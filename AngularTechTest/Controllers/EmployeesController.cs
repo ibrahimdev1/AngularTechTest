58f4e05af8cd5cafc8a98780477c6f1022f2838e
ibraheemdev1 <ibraheem.barhom52@gmail.com>
2022-03-01 16:15:00 +0200

    Add project files.

---
+++ AngularTechTest/Controllers/EmployeesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using AngularTechTest.Models;
+namespace AngularTechTest.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        // GET: Employees
+        public ActionResult Index()
+        {
+         
+            return View();
+        }
+        [HttpGet]
+        public JsonResult GetEmpl()
+
+        {
+
+            EmployeesContext employeesContext = new EmployeesContext();
+            var resault = employeesContext.Employees.ToList();
+
+            return Json(resault, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public JsonResult UpdateEmpl(int id , int salary  )
+
+        {
+            
+            EmployeesContext employeesContext = new EmployeesContext();
+            employeesContext.Employees.Single(emp=>emp.Id==id ).Salary = salary;
+            employeesContext.SaveChanges();
+            return Json(employeesContext, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ AngularTechTest/Scripts/filejs.js
@@ -0,0 +1,85 @@
+﻿/// <reference path="dirpagination.js" />
+/// <reference path="angular.min.js" />
+var MyApp = angular.module('MyApp', ['angularUtils.directives.dirPagination']);
+
+MyApp.controller('EmpCtrl', function ($scope, $http, EmployeeService) {
+    $scope.currentPage = 1;
+    $scope.pageSize = 5;
+
+    $scope.reload = ()=> window.location.reload(); 
+
+    $scope.ButtonClick = function () {
+          var post =$http({
+            method: "POST",
+            url: "/Employees/UpdateEmpl",
+            dataType: 'json',
+            params: { id: $scope.EditEmp.Id, salary: $scope.EditEmp.Salary },
+        }).then()
+        {
+            window.location.reload();
+        }
+    };
+
+
+    $scope.getTotal = function gettotal() {
+        var total = 0
+        for (var i = 0; i < $scope.Employees.length; i++) {
+            total += $scope.Employees[i].Salary
+        }
+        return total
+    };
+    $scope.getemp = function (emp) {
+        $scope.EditEmp = emp;
+
+
+    };
+
+
+
+
+    getEmployee();
+
+    function getEmployee() {
+
+
+        EmployeeService.getEmployee()
+
+            .then(function (response) {
+
+                $scope.Employees = response.data;
+
+
+            })
+
+            , (function (error) {
+
+                $scope.status = 'Unable to load customer data: ' + error.message;
+
+                console.log($scope.status);
+
+            });
+
+
+    }
+
+});
+
+
+
+MyApp.factory('EmployeeService', ['$http', function ($http) {
+
+
+
+    var EmployeeService = {};
+
+    EmployeeService.getEmployee = function () {
+
+        return $http.get('/Employees/GetEmpl');
+
+    };
+
+    return EmployeeService;
+
+
+
+}]);
